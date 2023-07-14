2ad2d4766f4dc09edb164352c42954b1c9277c8b
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-07-14 13:21:49 -0500

    NOSEXD, creo que los demas metodos

--- backend/controllers/tipoDinero.controllers.js
+++ backend/controllers/tipoDinero.controllers.js
@@ -5,4 +5,54 @@ const obtenerDinero = async(req,res)=>{
     res.json(TipoDineros)
 }
 
-export {obtenerDinero}
+
+const agregarCategoria = async (req,res) => {
+
+    const tipodinero = new TipoDinero(req.body)
+
+    try {
+        const nuevaTipoDinero = await tipodinero.save();
+
+        res.json(nuevaTipoDinero);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const borrarCategoria = async (req,res) =>{
+    try {
+        await TipoDinero.deleteOne({_id:req.params.id});
+        res.status(204).send();
+    } catch (error) {
+        res.status(404);
+        res.send({error:"TipoDinero No Existe"})
+    }
+}
+
+const actualizarCategoria = async (req,res) =>{
+    try {
+        const categoria = await TipoDinero.findOne({_id:req.params.id})
+
+        if(req.body.categoria){
+            categoria.categoria = req.body.categoria;
+        }
+
+        if(req.body.producto){
+            categoria.producto = req.body.producto;
+        }
+
+        if(req.body.talla){
+            categoria.talla = req.body.talla;
+        }
+
+        await categoria.save();
+        res.send(categoria)
+    } catch (error) {
+        res.status(404),
+        res.status({error:"categoria no existe"})
+    }
+}
+
+export {obtenerDinero,agregarCategoria,borrarCategoria,actualizarCategoria}
+
